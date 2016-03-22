cf0f147dc244b3c2618b9708e21f826c80f250b6
TGF2user <tgf2user@gmail.com>
2016-03-22 13:28:01 +0100

    Added files and stuff

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace program
+{
+    class Program
+    {
+        public static void Rendez(List<Versenyzo> be)
+        {
+            Versenyzo Csere = new Versenyzo(); //Létrehoz egy ideiglenes változót
+
+            #region Sorbarendezés
+            for (int i = 0; i < be.Count-1; i++)
+            {
+                int index = i;
+                int ertek = be[i].getVersenyIdo();
+                for (int j = i + 1; j < be.Count; j++)
+                {
+                    if (be[j].getVersenyIdo() < ertek) 
+                    {
+                        ertek = be[j].getVersenyIdo();
+                        index = j; 
+                    }
+                }
+                Csere = be[i];
+                be[i] = be[index];
+                be[index] = Csere;
+            }
+            #endregion
+        }
+
+        static void Main(string[] args)
+        {
+            List<Versenyzo> Versenyzok = new List<Versenyzo>();
+
+            using (StreamReader FáljOlvasó = new StreamReader("TRIATLON.BE", Encoding.UTF8))
+            {
+                
+                while (!FáljOlvasó.EndOfStream)
+                {
+                    //Ideiglenes változóba beteszi az értékeket, majd beteszi a listába
+                    Versenyzo Ideiglenes = new Versenyzo();
+                    Ideiglenes.Nev = FáljOlvasó.ReadLine();
+                    Ideiglenes.UI = int.Parse(FáljOlvasó.ReadLine());
+                    Ideiglenes.KI = int.Parse(FáljOlvasó.ReadLine());
+                    Ideiglenes.FI = int.Parse(FáljOlvasó.ReadLine());
+
+                    Versenyzok.Add(Ideiglenes);
+                }
+            }
+
+            Rendez(Versenyzok); //Rendez versenyidő szerint
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("{0}",Versenyzok[i].Nev); //Kiírja a top 3-at versenyidő szerint
+            }
+
+            Console.WriteLine();
+            //kiírja a képernyőre az úszási, kerékpározási, futási átlagsebességet
+            Console.WriteLine("{0}\nÚszás átlagsebesség: {1} km/h\nKerékpár átlagsebesség: {2} km/h\nFutás átlagsebesség: {3} km/h", Versenyzok[0].Nev, Versenyzok[0].getAtSebUsz(),Versenyzok[0].getAtSebKer(),Versenyzok[0].getAtSebFut());
+
+            using (StreamWriter FáljÍró = new StreamWriter("TRIATLON.KI")) //TRIATLON.KI-fáljba írja ki a formázott eredményeket
+            {
+                for (int i = 0; i < Versenyzok.Count; i++)
+                {
+                    FáljÍró.WriteLine(Versenyzok[i].Nev +" "+ Versenyzok[i].getIdoForm(Versenyzok[i].getVersenyIdo()));
+                }
+            }
+
+            using (StreamWriter FáljÍró = new StreamWriter("RESZER.KI")) //TRIATLON.KI-fáljba írja ki a formázott részeredményeket (Úszás, Kerékpározás, Futás)
+            {
+                #region Fáljba írás rendezéssel
+                int LKI = 0;
+
+                for (int i = 1; i < Versenyzok.Count; i++)
+                {
+                    if (Versenyzok[LKI].UI > Versenyzok[i].UI)
+                    {
+                        LKI = i;
+                    }
+                }
+
+                FáljÍró.WriteLine(Versenyzok[LKI].Nev + " " + Versenyzok[LKI].getIdoForm(Versenyzok[LKI].UI));
+
+                LKI = 0;
+                for (int i = 1; i < Versenyzok.Count; i++)
+                {
+                    if (Versenyzok[LKI].KI > Versenyzok[i].KI)
+                    {
+                        LKI = i;
+                    }
+                }
+
+                FáljÍró.WriteLine(Versenyzok[LKI].Nev + " " + Versenyzok[LKI].getIdoForm(Versenyzok[LKI].KI));
+
+                LKI = 0;
+                for (int i = 1; i < Versenyzok.Count; i++)
+                {
+                    if (Versenyzok[LKI].FI > Versenyzok[i].FI)
+                    {
+                        LKI = i;
+                    }
+                }
+
+                FáljÍró.WriteLine(Versenyzok[LKI].Nev + " " + Versenyzok[LKI].getIdoForm(Versenyzok[LKI].FI));
+                #endregion
+            }
+
+            Console.ReadKey();//Program lefutása után vár egy gomblenyomásra
+        }
+    }
+}
---
+++ Versenyzo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class Versenyzo
+    {
+        public string Nev;
+        public int UI = 0, KI = 0, FI = 0;
+
+        public Versenyzo(string Nev, int UI, int KI, int FI)
+        {
+            this.Nev = Nev;
+            this.UI = UI;
+            this.KI = KI;
+            this.FI = FI;
+        }
+
+        public Versenyzo()
+        {
+ 
+        }
+
+        public int getVersenyIdo() //Visszaadja az egész versenyidőt másodpercekben
+        {
+            return UI + KI + FI;
+        }
+
+        public double getAtSebUsz() //Visszaadja az átlagsebességet az úszásnál
+        {
+            return Math.Round((1.5d / (UI / 3600d)),2);
+        }
+
+        public double getAtSebKer() //Visszaadja az átlagsebességet a kerékpározásnál
+        {
+            return Math.Round((40 / (KI / 3600d)), 2);
+        }
+
+        public double getAtSebFut() //Visszaadja az átlagsebességet a futásnál
+        {
+            return Math.Round((10/ (FI / 3600d)), 2);
+        }
+
+        public string getIdoForm(int Idő) //Másodpercekből egz Óra:Perc:Másodperc formátumot ad vissza
+        {
+            string Ki = "";
+            //int ido = UI + KI + FI;
+            string Ora = Math.Floor(Idő/3600d).ToString();
+            string Perc = Math.Floor((Idő % 3600d)/60d).ToString();
+            string Másodperc = (Idő % 60).ToString();
+
+            if (Ora.Length != 2) Ora = "0" + Ora;
+            if (Perc.Length != 2) Perc = "0" + Perc;
+            if (Másodperc.Length != 2) Másodperc = "0" + Másodperc;
+
+            Ki = Ora + ":" + Perc + ":" + Másodperc;
+
+            return Ki;
+        }
+
+    }
+}
